2d336efbe50f87360698cc549fbc87b73228cd38
hiennv <hiennv@rikkeisoft.com>
2018-12-18 15:48:59 +0700

    Edit NFCReader

--- app/src/main/java/com/example/hiennv/rfidexample/MainActivity.java
+++ app/src/main/java/com/example/hiennv/rfidexample/MainActivity.java
@@ -4,24 +4,28 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private NfcAdapter nfcAdapter;
     TextView tvInfo;
     TextView tvTagInfo;
-
+    TextView tvBlock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvInfo = findViewById(R.id.tv_info);
         tvTagInfo = findViewById(R.id.tv_tag_info);
+        tvBlock = findViewById(R.id.tv_block);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         if (nfcAdapter == null) {
@@ -115,5 +119,85 @@ public class MainActivity extends AppCompatActivity {
         strTypeInfo += "SectorCount: " + sectorCount + "\n";
 
         tvTagInfo.setText(strTypeInfo);
+        new ReadMifareClassicTask(mifareClassicTag).execute();
+    }
+
+    private class ReadMifareClassicTask extends AsyncTask<Void, Void, Void> {
+
+        /*
+        MIFARE Classic tags are divided into sectors, and each sector is sub-divided into blocks.
+        Block size is always 16 bytes (BLOCK_SIZE). Sector size varies.
+        MIFARE Classic 1k are 1024 bytes (SIZE_1K), with 16 sectors each of 4 blocks.
+        */
+
+        MifareClassic taskTag;
+        int numOfBlock;
+        final int FIX_SECTOR_COUNT = 16;
+        boolean success;
+        final int numOfSector = 16;
+        final int numOfBlockInSector = 4;
+        byte[][][] buffer = new byte[numOfSector][numOfBlockInSector][MifareClassic.BLOCK_SIZE];
+
+        ReadMifareClassicTask(MifareClassic tag){
+            taskTag = tag;
+            success = false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            tvBlock.setText("Reading Tag, don't remove it!");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                taskTag.connect();
+
+                for(int s=0; s<numOfSector; s++){
+                    if(taskTag.authenticateSectorWithKeyA(s, MifareClassic.KEY_DEFAULT)) {
+                        for(int b=0; b<numOfBlockInSector; b++){
+                            int blockIndex = (s * numOfBlockInSector) + b;
+                            buffer[s][b] = taskTag.readBlock(blockIndex);
+                        }
+                    }
+                }
+
+                success = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally{
+                if(taskTag!=null){
+                    try {
+                        taskTag.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            //display block
+            if(success){
+                String stringBlock = "";
+                for(int i=0; i<numOfSector; i++){
+                    stringBlock += i + " :\n";
+                    for(int j=0; j<numOfBlockInSector; j++){
+                        for(int k=0; k<MifareClassic.BLOCK_SIZE; k++){
+                            stringBlock += String.format("%02X", buffer[i][j][k] & 0xff) + " ";
+                        }
+                        stringBlock += "\n";
+                    }
+                    stringBlock += "\n";
+                }
+                tvBlock.setText(stringBlock);
+            }else{
+                tvBlock.setText("Fail to read Blocks!!!");
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/hiennv/rfidexample/senddata/SendDataActivity.java
@@ -0,0 +1,96 @@
+package com.example.hiennv.rfidexample.senddata;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.hiennv.rfidexample.R;
+
+import java.io.File;
+
+public class SendDataActivity extends AppCompatActivity {
+    public static final String TAG = SendDataActivity.class.getSimpleName();
+    Button btnSendData;
+    private NfcAdapter mNfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_data);
+        btnSendData = findViewById(R.id.btn_send_file);
+        PackageManager pm = this.getPackageManager();
+        // Check whether NFC is available on device
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            // NFC is not available on the device.
+            Toast.makeText(this, "The device does not has NFC hardware.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        // Check whether device is running Android 4.1 or higher
+        else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            // Android Beam feature is not supported.
+            Toast.makeText(this, "Android Beam is not supported.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            // NFC and Android Beam file transfer is supported.
+            Toast.makeText(this, "Android Beam is supported on your device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        btnSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFile();
+            }
+        });
+    }
+
+    private void sendFile() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        // Check whether NFC is enabled on device
+        if(!mNfcAdapter.isEnabled()){
+            // NFC is disabled, show the settings UI
+            // to enable NFC
+            Toast.makeText(this, "Please enable NFC.",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+        }
+        // Check whether Android Beam feature is enabled on device
+        else if(!mNfcAdapter.isNdefPushEnabled()) {
+            // Android Beam is disabled, show the settings UI
+            // to enable Android Beam
+            Toast.makeText(this, "Please enable Android Beam.",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
+        }
+        else {
+            // NFC and Android Beam both are enabled
+
+            // File to be transferred
+            // For the sake of this tutorial I've placed an image
+            // named 'wallpaper.png' in the 'Pictures' directory
+            String fileName = "wallpaper.png";
+
+            // Retrieve the path to the user's public pictures directory
+            File fileDirectory = Environment
+                    .getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES);
+
+            // Create a new file using the specified directory and name
+            File fileToTransfer = new File(fileDirectory, fileName);
+            fileToTransfer.setReadable(true, false);
+
+            mNfcAdapter.setBeamPushUris(
+                    new Uri[]{Uri.fromFile(fileToTransfer)}, this);
+        }
+    }
+}
